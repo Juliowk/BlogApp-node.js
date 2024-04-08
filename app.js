68ff03b9afa75c74f2ff3895e10ad1d570c23139
Júlio Elias de Sousa rocha <julioeliasdesousarocha.work@gmail.com>
2024-04-08 19:44:04 -0300

    Vercel file

--- app.js
+++ app.js
@@ -168,5 +168,5 @@ app.get("/404", (req, res) => {
 app.use('/adm', routerAdmin);
 app.use('/usuarios', routerUsuarios);
 
-const PORT = 3000;
-app.listen(PORT, () => { console.log(`APLICAÇÃO RODANDO EM: http://localhost:3000/`) });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => { console.log(`APLICAÇÃO RODANDO!`) });
